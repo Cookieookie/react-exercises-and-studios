55740505110f1d1da208cfcae552f64436ba07cf
Cookie Van <cookievan03@gmail.com>
2023-07-24 21:25:28 -0500

    finished booklist component requirements

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,5 +1,5 @@
 export default function BookList() {
-   let pageTitle = "shrug";
+   let pageTitle = "Books I Read This Month";
    let book1 = "google.com";
    let book2 = "";
    let book3 = "";
@@ -7,9 +7,9 @@ export default function BookList() {
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={"https://upload.wikimedia.org/wikipedia/en/1/10/The_Cat_in_the_Hat.png"} alt="The Cat in The Hat" />
+         <img src={"https://www.marxistbooks.com/cdn/shop/products/ManifestoforMBooks_e47fb769-8d30-45c9-8aae-123b8c305efa_large.png?v=1652363817"} alt="The Communist Manifesto" />
+         <img src={"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554006152l/6._SX318_.jpg"} alt="Harry Potter and the Goblet of Fire" />
       </div>      
    );
 }
