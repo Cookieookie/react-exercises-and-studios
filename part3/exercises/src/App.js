ee5a5468ae9df96b55758c23f989e899a8849793
Cookie Van <cookievan03@gmail.com>
2023-09-09 16:45:20 -0500

    finished exercises from part 3

---
+++ part3/exercises/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import MyProjects from './components/Projects';
+import HobbyIntroduction from './components/Introduction';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Hobby: Painting</h1>
+      <HobbyIntroduction />
+      <MyProjects />
+    </div>
+  );
+}
+
+export default App;
---
+++ part3/exercises/src/components/Introduction.js
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export default function HobbyIntroduction () {
+    return(
+    <div>
+    <h2>3 Fun Facts About this Hobby</h2>
+      <ol>
+         <li>It's relaxing</li>
+         <li>It's creative</li>
+         <li>It's fun</li>
+      </ol>
+    </div>
+    );
+}
